6c2b44aeadce5d9e5b2921b9490136cbcf052fec
akkayasoft <akkayasoft@gmail.com>
2025-09-02 07:17:52 +0300

    final projesi oluştu

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "ayhanakkaya_ibm_react_final_project",
+  plugins: [react()],
+})
